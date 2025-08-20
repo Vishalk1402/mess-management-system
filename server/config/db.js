9a688f2d865b1800f66d33f43eb994f1c266f500
Vishal Koli <Vishalkoli1402@gmail.com>
2025-08-21 01:10:34 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,10 @@
 import mysql from "mysql2";
 import fs from "fs";
-import path from "path";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const CERT_PATH = path.resolve('./server/certs/temp-ca.pem');
-
-// Write the PEM file if SSL is enabled
-if (process.env.DB_SSL === 'true') {
-  fs.mkdirSync(path.dirname(CERT_PATH), { recursive: true });
-  fs.writeFileSync(
-    CERT_PATH,
-    process.env.DB_CA_PEM.replace(/\\n/g, '\n') // Converts single-line PEM to proper format
-  );
-}
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -22,8 +12,8 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_SSL === 'true' ? {
-    ca: fs.readFileSync(CERT_PATH),
+ssl: process.env.DB_SSL === 'true' ? {
+    ca: fs.readFileSync('./certs/ca.pem'),
     rejectUnauthorized: true
   } : undefined
 });
