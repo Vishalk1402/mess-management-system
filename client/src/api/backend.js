67e13bd4ccacea0746ad18627b5b55df51a0dcbc
Vishal Koli <Vishalkoli1402@gmail.com>
2025-08-23 12:28:46 +0530

    Update backend.js

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : "https://mess-management-system-2.onrender.com";
+    : "https://mess-management-system-2.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
