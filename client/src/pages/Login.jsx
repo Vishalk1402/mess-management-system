4656adc58505cac7a5104124e21c7d4b8e272a22
Vishal Koli <Vishalkoli1402@gmail.com>
2025-08-23 22:07:33 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {  toast } from "react-toastify";
+
 import { loginUser } from "../api/backend"; // ✅ use centralized API
 
 function Login() {
@@ -89,9 +89,6 @@ function Login() {
           </a>
         </p>
       </form>
-
-      {/* ✅ Toast container */}
-      <ToastContainer />
     </div>
   );
 }
