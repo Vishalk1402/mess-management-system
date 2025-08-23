101b9410a3b0733ea59d95825ad744678e64a66b
Vishal Koli <Vishalkoli1402@gmail.com>
2025-08-23 12:25:27 +0530

    Update backend.js

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : "https://mess-management-system-ayv2.onrender.com/api";
+    : "https://mess-management-system-2.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -53,5 +53,6 @@ export const addNotice = (noticeData) => api.post("/notice/addnotice", noticeDat
 export const deleteNotice = (id) => api.delete(`/notice/${id}`);
 
 export default api;
+
 
 
