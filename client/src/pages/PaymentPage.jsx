7f37c05d022d149b603e2c2cc1e9968b62731e9e
Vishal Koli <Vishalkoli1402@gmail.com>
2025-08-23 22:08:04 +0530

    Update PaymentPage.jsx

--- client/src/pages/PaymentPage.jsx
+++ client/src/pages/PaymentPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import api from "../api/backend"; // ✅ use your api.js wrapper
+import { toast } from "react-toastify";
+
+import api from "../api/backend"; 
 
 // Helper to load Razorpay script
 const loadRazorpayScript = () => {
@@ -97,7 +97,6 @@ function PaymentPage() {
           🚀 Pay Now
         </button>
       </div>
-      <ToastContainer />
     </div>
   );
 }
